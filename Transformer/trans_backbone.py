36b073733138efde11ec351dc59c33a0cea21293
Matt Majeske <matthew.majeske@gmail.com>
2026-04-08 22:19:42 -0400

    new stuff, honestly I think my branch is addicted to updating itself

--- Transformer/trans_backbone.py
+++ Transformer/trans_backbone.py
@@ -1,5 +1,7 @@
 import torch
 
-
+from  trans_blocks import SinusoidalPositionalEncoding, MultiHeadSelfAttention
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
+
+
--- Transformer/trans_blocks.py
+++ Transformer/trans_blocks.py
@@ -90,8 +90,7 @@ class SinusoidalPositionalEncoding(nn.Module):
 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) *
-                             (-math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)  # (1, max_len, d_model)
